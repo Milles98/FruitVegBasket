0433634354810f583af176a9a12336f5945aa07a
Mille Elfver <mille.elfver98@gmail.com>
2024-06-24 10:23:29 +0200

    Added seeding for categories with images and credit

--- FruitVegBasket/Models/Category.cs
+++ FruitVegBasket/Models/Category.cs
@@ -2,9 +2,19 @@
 
 public class Category
 {
+    public Category(short id, string name, short parentId, string image, string credit)
+    {
+        Id = id;
+        Name = name;
+        Image = image;
+        Credit = credit;
+        ParentId = parentId;
+    }
     public short Id { get; set; }
     public string Name { get; set; }
     public string Image { get; set; }
     public short ParentId { get; set; }
+    public string? Credit { get; }
+    
     public bool IsMainCategory => ParentId == 0;
 }
