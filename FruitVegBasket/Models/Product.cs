7e78c18b7de4d03afafeb4aadeb97c7f525e5419
Mille Elfver <mille.elfver98@gmail.com>
2024-06-24 13:53:48 +0200

    Added offer to main page for codes

--- FruitVegBasket/Models/Product.cs
+++ FruitVegBasket/Models/Product.cs
@@ -6,7 +6,7 @@ public class Product
     public string Name { get; set; }
     public string Image { get; set; }
     public decimal Price { get; set; }
-    
+
     public short CategoryId { get; set; }
     public string CategoryName { get; set; }
 }
