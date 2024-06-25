1af350188a694bd6b36df1c19656f7b81a89c517
Mille Elfver <mille.elfver98@gmail.com>
2024-06-25 07:56:20 +0200

    Added httpclient to CategoryService.cs, also added OfferService

---
+++ FruitVegBasket/Services/OfferService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using FruitVegBasket.Constants;
+using FruitVegBasket.Models;
+
+namespace FruitVegBasket.Services;
+
+public class OfferService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public OfferService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IEnumerable<Offer>> GetActiveOffersAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient(AppConstants.HttpClientName);
+        var response = await httpClient.GetAsync("/masters/offers");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            if (!string.IsNullOrEmpty(content))
+            {
+                return JsonSerializer.Deserialize<IEnumerable<Offer>?>(content);
+            }
+        }
+
+        return Enumerable.Empty<Offer>();
+    }
+}
